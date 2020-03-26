1df57d75fafcf27644a091b5d64d27ef8789df7e
frankkode <masasimpafra@gmail.com>
2020-03-26 21:08:07 +0100

    add readme file, travis fale and mysql database

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.com/frankkode/gym-max-products.svg?branch=master)](https://travis-ci.com/frankkode/gym-max-products)
+
+# [GYM-MAX&PRODUCTS](https://gym-max-products.herokuapp.com/)
+
+
--- gym_max/settings.py
+++ gym_max/settings.py
@@ -83,15 +83,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gym_max.wsgi.application'
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#   }
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print('Database URL not found. Using SQLite instead')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
